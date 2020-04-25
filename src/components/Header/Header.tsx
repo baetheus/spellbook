3a2c5301df54553bba682f4b70fb8f75347ca7c1
Brandon Blaylock <brandon@null.pub>
2020-04-25 11:09:32 -0700

    fix: minor style edits
    * reorder utility classes and custom reset
    * square off about page icon
    * fix focus glow shadow cutoff in header
    * drop up button to bottom of page
    * change focus to use focus theme
    * add color transitions to make focus and select prettier

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,23 @@
+import "./Header.scss";
+
 import { h } from "preact";
 import { Link } from "preact-router";
 
 import { MdHelp } from "react-icons/md";
 
 export const Header = () => (
-  <header class="fld-row flg-4 ai-ctr jc-spb vhmn-3 ovx-au pwr-5">
+  <header class="fld-row flg-4 ai-ctr jc-spb vhmn-3">
     <h1>
-      <Link href="/#" class="pwx-3 bra-1">
+      <Link href="/#" class="bra-1">
         Spellbook
       </Link>
     </h1>
-    <nav class="fld-row flg-3 ai-ctr">
-      <Link href="/about" aria-label="Help and About page link" class="bra-1 fs-u3 trns-d3">
+    <nav class="fld-row flg-3 ai-ctr pwx-3">
+      <Link
+        href="/about"
+        aria-label="Help and About page link"
+        class="about-icon bra-1 fs-u4 pwx-2"
+      >
         <MdHelp />
       </Link>
     </nav>

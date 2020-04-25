feb8aa44f0eb3ac52a8a88f3dcfc91d103e93bdb
Brandon Blaylock <brandon@null.pub>
2020-04-25 16:29:15 -0700

    chore: remove meyer reset
    * Remove vw-vw100 from footer
    * Remove vw-p100 from main in DefaultLayout
    * Add width and height to html/body
    * Add * { margin: 0; padding: 0 } as new reset

--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -5,7 +5,7 @@ import { Footer } from "../Footer";
 
 export const DefaultLayout: FunctionalComponent<{}> = ({ children }) => {
   return (
-    <main class="vhmn-vh100 vw-p100 vwcmx-em1 vwc-p100 fld-col ai-ctr  pwx-4 ova-vi">
+    <main class="vhmn-vh100 vwcmx-em1 vwc-p100 fld-col ai-ctr pwx-4">
       <Header />
       <section class="fls-1-1 fld-col flg-4 ai-stc">{children}</section>
       <Footer className="brt-1" />
